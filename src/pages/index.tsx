8c1231c28d179bf7798be97558bf757832429e4c
mari <mari@hakke.ro>
2020-12-27 20:32:57 +0000

    Add a split flap display on the frontpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,59 @@
 import React from 'react';
-import { Logo } from '~/components/Logo';
 import { Layout } from '~/components/Layout';
 import styled from '@emotion/styled';
+import { SplitFlapWord } from '~/components/SplitFlap';
 
-const Hero = styled(Logo)`
-  width: 600px;
-  .text-path {
-    fill: #fff;
-    stroke: #0f0;
-    stroke-width: 2px;
-
-    &.stroke {
-      fill: #000;
-    }
-  }
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(24, 20px [col]);
+  grid-template-rows: repeat(auto-fit, 20px);
+  height: fit-content;
+  align-self: center;
 `;
 
-const Home = () => (
-  <Layout>
-    <Hero />
-  </Layout>
-);
+const Word = styled(SplitFlapWord)`
+  font-family: Courier;
+  display: block;
+  font-size: 16px;
+  text-align: center;
+
+  color: ${props => {
+    if (props.word.includes('CARCOSA')) {
+      return '#f00';
+    }
+    return '#fff';
+  }};
+
+  text-shadow: 0 0 10px #fff, 0 0 4px #f00;
+`;
+
+const Home = () => {
+  const quote = Array.from(
+    `I saw the lake of Hali, thin and blank, without a ripple or wind to stir it, and I saw the towers of Carcosa behind the moon. Aldebaran, the Hyades, Alar, Hastur, glided through the cloud-rifts which fluttered and flapped as they passed like the scolloped tatters of the King in Yellow.`
+  )
+    .map(s => s.toUpperCase())
+    .filter(s => {
+      const charCode = s.charCodeAt(0);
+      return charCode <= 93 && charCode >= 32;
+    })
+    .join('')
+    .split(' ');
+
+  const splitFlapDisplay = quote.map((s: string, i: number) => (
+    <Word
+      word={`${s} `}
+      key={`word-${i}`}
+      delay={i * 200}
+      delaySteps={100}
+      speed={20}
+    />
+  ));
+
+  return (
+    <Layout>
+      <Grid>{splitFlapDisplay}</Grid>
+    </Layout>
+  );
+};
 
 export default Home;
