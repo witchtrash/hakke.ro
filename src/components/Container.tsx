6567fda8eca5d4460324053bc39ed03771c6d666
mari <mari@hakke.ro>
2022-02-07 13:42:05 +0000

    Implement front page decorations

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, BoxProps } from '@chakra-ui/react';
 
-export const Main = ({ children, ...rest }: BoxProps) => (
+export const Container = ({ children, ...rest }: BoxProps) => (
   <Box p={[6, 12, 16]} h="100vh" w="100vw" {...rest}>
     {children}
   </Box>
