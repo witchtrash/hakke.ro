fea4bcf4f0eff52e993975cbb927fe657fbbbf1d
mari <mari@hakke.ro>
2022-02-08 20:49:39 +0000

    Update meta tags and og image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,26 +9,31 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <React.Fragment>
       <Head>
-        <title key="title">1-800-hakkero</title>
-        <meta property="og:title" content="hakke.ro" key="og-title" />
+        <title key="title">hakke.ro</title>
+        <meta
+          property="og:title"
+          content="javascript witchcraft"
+          key="og-title"
+        />
         <meta name="author" content="mari" key="author" />
 
         <link rel="icon" href="/icon.png" />
+        <meta name="theme-color" content="#6B2FB6" />
 
         <meta
           name="description"
-          content="Strange is the night where black stars rise, And strange moons circle through the skies, But stranger still is Lost Carcosa."
+          content="A website by an idiot."
           key="description"
         />
         <meta
           property="og:description"
-          content="Strange is the night where black stars rise, And strange moons circle through the skies, But stranger still is Lost Carcosa."
+          content="A website by an idiot."
           key="og-description"
         />
         <meta property="og:site_name" content="hakke.ro" />
         <meta property="og:type" content="website" key="og-type" />
         <meta property="og:url" content="https://hakke.ro" />
-        <meta property="og:image" content="/assets/og.webp" key="og-image" />
+        <meta property="og:image" content="/assets/og.jpg" key="og-image" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta
           property="twitter:image"
