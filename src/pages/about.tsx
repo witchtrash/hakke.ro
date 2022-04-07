6199c7c577d003976817b480eec61647f78eaa8f
mari <mari@hakke.ro>
2022-04-07 18:22:30 +0000

    Update the about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Head from 'next/head';
+import { InferGetServerSidePropsType } from 'next';
 import { PageLayout } from 'components/Layout';
 import {
   Link,
@@ -16,7 +18,6 @@ import { MotionBox } from 'components/MotionBox';
 import { RiHeartFill } from 'react-icons/ri';
 import { clientFactory } from 'util/supabase';
 import { BouncyLetter, ColorText } from 'components/About';
-import Head from 'next/head';
 
 interface Artist {
   id: number;
@@ -25,43 +26,10 @@ interface Artist {
   scrobble_count: number;
 }
 
-const About = () => {
-  const [scrobbles, setScrobbles] = React.useState<undefined | number>();
-  const [artists, setArtists] = React.useState<Artist[] | undefined>();
-
-  React.useEffect(() => {
-    const client = clientFactory({ useServiceKey: false });
-
-    client
-      .from<{
-        id: number;
-        scrobble_count: number;
-      }>('scrobbles')
-      .select('id, scrobble_count')
-      .order('id', {
-        ascending: false,
-      })
-      .limit(1)
-      .then(res => {
-        if (res.data) {
-          setScrobbles(res.data[0].scrobble_count);
-        }
-      });
-
-    client
-      .from<Artist>('weekly_artists')
-      .select('id, artist_name, scrobble_count, lastfm_url')
-      .order('id', {
-        ascending: false,
-      })
-      .limit(5)
-      .then(res => {
-        if (res.data) {
-          setArtists(res.data.reverse());
-        }
-      });
-  }, []);
-
+const About = ({
+  scrobbleCount,
+  artists,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <React.Fragment>
       <Head>
@@ -96,11 +64,11 @@ const About = () => {
                   {`i get paid to do that.`}
                 </Text>
                 <Pause ms={300} />
-                <Text fontFamily="heading">{`here are some things about me.`}</Text>
+                <Text>{`here are some things about me.`}</Text>
                 <Pause ms={300} />
               </Box>
 
-              <Box py="3">
+              <Box py="3" pr="3">
                 <UnorderedList fontFamily="mono">
                   <ListItem>
                     {`education: `}
@@ -125,31 +93,31 @@ const About = () => {
                 </UnorderedList>
               </Box>
 
-              <Box py="3">
-                <Text>
-                  {`i like listening to bad music, `}
+              {artists && scrobbleCount ? (
+                <Box py="3">
+                  <Text>
+                    {`i like listening to bad music, `}
+                    <Pause ms={300} />
+                    {`i have like `}
+                    <Pause ms={300} />
+                    <ColorText color="pink.400">
+                      {scrobbleCount}
+                      {` scrobbles`}
+                    </ColorText>
+                    {` on my `}
+                    <Link
+                      color="violet.400"
+                      href="https://www.last.fm/user/witchtrash"
+                    >
+                      {`last.fm. `}
+                    </Link>
+                    <Pause ms={300} />
+                    {`wowie.`}
+                  </Text>
                   <Pause ms={300} />
-                  {`i have like `}
-                  <Pause ms={300} />
-                  <ColorText color="pink.400">
-                    {scrobbles}
-                    {` scrobbles`}
-                  </ColorText>
-                  {` on my `}
-                  <Link
-                    color="violet.400"
-                    href="https://www.last.fm/user/witchtrash"
-                  >
-                    {`last.fm. `}
-                  </Link>
-                  <Pause ms={300} />
-                  {`wowie.`}
-                </Text>
-                <Pause ms={300} />
-                <Text>
-                  {`lately ive been listening to `}
-                  <Pause ms={300} />
-                  {artists ? (
+                  <Text>
+                    {`lately ive been listening to `}
+                    <Pause ms={300} />
                     <React.Fragment>
                       {artists.slice(0, 4).map(artist => (
                         <React.Fragment key={artist.id}>
@@ -163,7 +131,7 @@ const About = () => {
                       <Link color="violet.400" href={artists[4].lastfm_url}>
                         {`${artists[4].artist_name}.`}
                       </Link>
-                      <Flex>
+                      <Flex flexWrap="wrap">
                         <Text>{`check em out by clicking the names, if you want`}</Text>
                         <Pause ms={300} />
                         <Box ml="1">
@@ -172,12 +140,10 @@ const About = () => {
                       </Flex>
                       <Pause ms={300} />
                     </React.Fragment>
-                  ) : (
-                    <React.Fragment>{`???`}</React.Fragment>
-                  )}
-                  <Pause ms={300} />
-                </Text>
-              </Box>
+                    <Pause ms={300} />
+                  </Text>
+                </Box>
+              ) : null}
 
               <Box py="3">
                 <Text>
@@ -227,6 +193,36 @@ const About = () => {
       </PageLayout>
     </React.Fragment>
   );
+};
+
+export const getServerSideProps = async () => {
+  const client = clientFactory({ useServiceKey: false });
+
+  const scrobbleData = await client
+    .from<{
+      id: number;
+      scrobble_count: number;
+    }>('scrobbles')
+    .select('id, scrobble_count')
+    .order('id', {
+      ascending: false,
+    })
+    .limit(1);
+
+  const artistData = await client
+    .from<Artist>('weekly_artists')
+    .select('id, artist_name, scrobble_count, lastfm_url')
+    .order('id', {
+      ascending: false,
+    })
+    .limit(5);
+
+  return {
+    props: {
+      scrobbleCount: scrobbleData.data?.[0].scrobble_count,
+      artists: artistData.data?.reverse(),
+    },
+  };
 };
 
 export default About;
