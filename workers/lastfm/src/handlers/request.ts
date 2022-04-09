78fe8efc5a9017163cf55f94458675065ee2ddf1
mari <mari@hakke.ro>
2022-04-09 03:54:28 +0000

    Add CORS handling to the Last.fm worker

--- workers/lastfm/src/handlers/request.ts
+++ workers/lastfm/src/handlers/request.ts
@@ -1,6 +1,16 @@
 import { Store } from '../util/store';
 
-export const handleRequest = async (): Promise<Response> => {
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,OPTIONS',
+  'Access-Control-Max-Age': '86400',
+};
+
+export const handleRequest = async (request: Request): Promise<Response> => {
+  if (request.method === 'OPTIONS') {
+    return handleOptions(request);
+  }
+
   const scrobbleCount = await STATS.get(Store.SCROBBLE_COUNT);
   const weeklyArtists = await STATS.get(Store.WEEKLY_ARTISTS);
 
@@ -12,8 +22,34 @@ export const handleRequest = async (): Promise<Response> => {
     {
       headers: {
         'Content-Type': 'application/json',
+        ...corsHeaders,
       },
       status: 200,
     }
   );
+};
+
+const handleOptions = (request: Request) => {
+  // Make sure the necessary headers are present
+  // for this to be a valid pre-flight request
+  const headers = request.headers;
+  if (
+    headers.get('Origin') !== null &&
+    headers.get('Access-Control-Request-Method') !== null &&
+    headers.get('Access-Control-Request-Headers') !== null
+  ) {
+    // Handle CORS pre-flight request.
+    return new Response(null, {
+      headers: corsHeaders,
+      status: 204,
+    });
+  } else {
+    // Handle standard OPTIONS request.
+    return new Response(null, {
+      headers: {
+        Allow: 'GET, OPTIONS',
+      },
+      status: 204,
+    });
+  }
 };
--- workers/lastfm/src/index.ts
+++ workers/lastfm/src/index.ts
@@ -1,7 +1,7 @@
 import { handleRequest, handleSchedule } from './handlers';
 
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest());
+  event.respondWith(handleRequest(event.request));
 });
 
 addEventListener('scheduled', event => {
